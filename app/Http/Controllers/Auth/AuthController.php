317ab9716252248cd0d23b0b12721b9a7445f79d
Md.Raysu kobir <86109661+raysulkobir@users.noreply.github.com>
2023-12-10 00:24:51 +0600

    Initial commit

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Session;
+use App\Models\User;
+use Hash;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
+
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function postLogin(Request $request){
+
+    }
+}
