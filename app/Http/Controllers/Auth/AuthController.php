ea9f715e7ab0a74afd7093dc170b3ce714209010
Md.Raysu kobir <86109661+raysulkobir@users.noreply.github.com>
2023-12-11 23:54:57 +0600

    logout work done

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -25,6 +25,42 @@ class AuthController extends Controller
     }
 
     public function postLogin(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
 
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')->withSuccess('You have Successfully loggedin');
+        }
+
+        return redirect("login")->withSuccess('Oppes! You have entered invalid credentials');
+    }
+
+    public function postRegistration(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        User::create($request->all());
+
+        return redirect("dashboard")->withSuccess('Great! You have Successfully loggedin');
+    }
+
+    public function dashboard(){
+        if (Auth::check()) {
+            return view('dashboard');
+        }
+
+        return redirect("login")->withSuccess('Opps! You do not have access');
+    }
+
+    public function logout(): RedirectResponse
+    {
+        Auth::logout();
+        return Redirect('login');
     }
 }
